89ee8b19d7f105598311d654781a0dbdee25a725
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-12-17 17:01:40 +0700

    Create page_php.php

---
+++ page_php.php
@@ -0,0 +1,58 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        kasetsart university sriracha campus
+    </h1>
+    <h1>
+        Faculty of Science, Sriracha
+    </h1>
+    <h1>
+        Computer Science and Information
+    </h1>
+    <h1>
+        Digital science and technology
+    </h1>
+
+    <h3> 10 + 10 = <?php echo 10 + 10; ?> </h3>
+    <h3> 10 - 10 = <?php echo 10 - 10; ?> </h3>
+    <h3> 10 * 10 = <?php echo 10 * 10; ?> </h3>
+    <h3> 10 / 10 = <?php echo 10 / 10; ?> </h3>
+    <h3> 10 % 10 = <?php echo 10 % 10; ?> </h3>
+    <h3> 10 ** 10 = <?php echo 10 ** 10; ?> </h3>
+
+    <h1>
+        ----------------------------------------------
+    </h1>
+
+    <h3> 5 == 10 ? <?php 
+    $b = 5 == 10 ? "= True" : "= False"; 
+    echo $b
+    ?> 
+    </h3>
+
+    <h3> 5 < 10 ? <?php 
+    $b = 5 < 10 ? "= True" : "= False"; 
+    echo $b
+    ?> 
+    </h3>
+
+    <h3> 5 != 10 ? <?php 
+    $b = 5 != 10 ? "= True" : "= False"; 
+    echo $b
+    ?> 
+    </h3>
+
+    <h3> 5+5 == 10 ? <?php 
+    $b = 5+5 == 10 ? "= True" : "= False"; 
+    echo $b
+    ?> 
+    </h3>
+</body>
+</html>
